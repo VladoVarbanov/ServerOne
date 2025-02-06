b26a3d6acbb42b066a8dd1f3856f7e7ec4617102
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2025-02-06 22:19:56 +0200

    Getting all Products and One product

--- Server Mongo DB/Server One/index.js
+++ Server Mongo DB/Server One/index.js
@@ -5,10 +5,33 @@ const app = express();
 
 app.use(express.json());
 
+// Get Home Page.
 app.get("/", (req, res) => {
   res.send("Hello from Node API Server Updated");
 });
 
+// Get All Products
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Get one Product.
+app.get("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Put Product in DB
 app.post("/api/products", async (req, res) => {
   try {
     const product = await Product.create(req.body);
