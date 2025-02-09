fd454fe6bf90fde5fe4fa642bd6db247a1baaf33
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2025-02-09 16:59:38 +0200

    Create routes

--- Server Mongo DB/Server One/index.js
+++ Server Mongo DB/Server One/index.js
@@ -1,44 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Product = require("./models/product.model.js");
+const productRoute = require("./routes/product.routes.js");
 const app = express();
 
+// Middleware.
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes.
+app.use("/api/products", productRoute);
 
 // Get Home Page.
 app.get("/", (req, res) => {
   res.send("Hello from Node API Server Updated");
-});
-
-// Get All Products
-app.get("/api/products", async (req, res) => {
-  try {
-    const products = await Product.find({});
-    res.status(200).json(products);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// Get one Product.
-app.get("/api/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-// Put Product in DB
-app.post("/api/products", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 });
 
 mongoose
---
+++ Server Mongo DB/Server One/routes/product.routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const Product = require("../models/product.model.js");
+const productController = require("../controllers/product.controller.js");
+const router = express.Router();
+
+// Get all products
+router.get("/", productController.getProducts);
+
+// Get single product
+router.get("/:id", productController.getProduct);
+
+// Create product
+router.post("/", productController.createProduct);
+
+// Update product
+router.put("/:id", productController.updatedProduct);
+
+// Delete product
+router.delete("/:id", productController.deleteProduct);
+
+module.exports = router;
