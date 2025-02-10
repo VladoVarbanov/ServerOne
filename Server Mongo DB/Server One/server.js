f743fd4dbcf2ac71c4203e5413ba7506ba8f459f
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2025-02-10 09:15:01 +0200

    Create user controller and user routes

--- Server Mongo DB/Server One/server.js
+++ Server Mongo DB/Server One/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const productRoute = require("./routes/product.routes.js");
+const userRoute = require("./routes/user.routes.js");
 const errorHandler = require("./middleware/errorHandler.js");
 const connectDb = require("./config/dbConnection.js");
 const app = express();
@@ -9,12 +10,13 @@ connectDb();
 const port = process.env.PORT || 5001;
 
 // Middleware.
-app.use(errorHandler);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(errorHandler);
 
 // Routes.
 app.use("/api/products", productRoute);
+app.use("/api/users", userRoute);
 
 // Start Server
 app.listen(port, () => {
